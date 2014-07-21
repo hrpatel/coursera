b7477783f1f88d3e54daf17a45e7eeb90da45bb5
hrpatel <pate3900+github@gmail.com>
2014-07-21 15:45:06 -0400

    week 5 homework q5

--- principles_of_computing/week5/p5.py
+++ principles_of_computing/week5/p5.py
@@ -1,5 +1,7 @@
 __author__ = 'hrpatel'
 
+COUNT = 0
+
 def memoized_fib(num, memo_dict):
     global COUNT
     COUNT += 1
@@ -11,3 +13,11 @@ def memoized_fib(num, memo_dict):
         sum2 = memoized_fib(num - 2, memo_dict)
         memo_dict[num] = sum1 + sum2
         return sum1 + sum2
+
+print memoized_fib(10, {0 : 0, 1 : 1})
+print COUNT
+print
+
+for i in range(1, 10):
+    COUNT = 0
+    print i, memoized_fib(i, {0 : 0, 1 : 1}), COUNT
