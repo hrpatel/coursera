0f68f03839dd6a7ae991623f4ba42c443383ac42
hrpatel <pate3900+github@gmail.com>
2014-07-21 15:35:25 -0400

    week 5 homework 4

--- principles_of_computing/week5/q4.py
+++ principles_of_computing/week5/q4.py
@@ -1,9 +1,33 @@
 __author__ = 'hrpatel'
 
+COUNT = 0
+
 def fib(num):
+    global COUNT
+    COUNT += 1
+
     if num == 0:
         return 0
     elif num == 1:
         return 1
     else:
         return fib(num - 1) + fib(num - 2)
+
+
+def fib_count(num):
+    if num == 0:
+        return 1
+    elif num == 1:
+        return 1
+    else:
+        return fib_count(num - 1) + fib_count(num - 2) + 1
+
+
+print fib(10)
+print COUNT
+
+
+for i in range(10):
+    COUNT = 0
+    fib(i)
+    print i, COUNT, fib_count(i)
