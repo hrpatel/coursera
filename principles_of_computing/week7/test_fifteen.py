ce644451600d9928a5371d32a5767a80f843ca80
hrpatel <pate3900+github@gmail.com>
2014-08-07 09:23:16 -0400

    update test case

--- principles_of_computing/week7/test_fifteen.py
+++ principles_of_computing/week7/test_fifteen.py
@@ -6,10 +6,10 @@ import fifteen_puzzle as puz
 obj = puz.Puzzle(3, 3, [[8, 7, 6],
                         [5, 4, 3],
                         [2, 1, 0]])
-assert obj.lower_row_invariant(2, 2) == True
+assert obj.lower_row_invariant(2, 2) is True
 
 # Test solve_interior_tile(...)
-obj = puz.Puzzle(3, 3, [[8, 7, 6],
-                        [5, 4, 3],
-                        [2, 1, 0]])
-obj.solve_interior_tile(2, 2)
+obj = puz.Puzzle(3, 3, [[0, 1, 2],
+                        [3, 4, 5],
+                        [6, 7, 8]])
+assert obj.row0_invariant(0) is True
