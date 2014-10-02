0289729d7ce463ea6f086229c8d655cc2230c1f5
hrpatel <pate3900+github@gmail.com>
2014-10-02 14:46:39 -0400

    code style

--- algorithmic_thinking/module3/project.py
+++ algorithmic_thinking/module3/project.py
@@ -13,18 +13,18 @@ where cluster_list is a list of clusters in the plane
 __author__ = 'hrpatel'
 
 
-import alg_cluster
-
-
 def pair_distance(cluster_list, idx1, idx2):
     """
     Helper function to compute Euclidean distance between two clusters in cluster_list with indices idx1 and idx2
 
     Returns tuple (dist, idx1, idx2) with idx1 < idx2 where dist is distance between cluster_list[idx1]
     and cluster_list[idx2]
+    :param cluster_list:
+    :param idx1:
+    :param idx2:
     :rtype : tuple
     """
-    return (cluster_list[idx1].distance(cluster_list[idx2]), min(idx1, idx2), max(idx1, idx2))
+    return cluster_list[idx1].distance(cluster_list[idx2]), min(idx1, idx2), max(idx1, idx2)
 
 
 def slow_closest_pairs(cluster_list):
@@ -34,6 +34,7 @@ def slow_closest_pairs(cluster_list):
     Returns the set of all tuples of the form (dist, idx1, idx2) where the cluster_list[idx1] and cluster_list[idx2]
     have minimum distance dist.
 
+    :param cluster_list:
     :rtype : set
     """
     # initialize return variable
@@ -66,9 +67,11 @@ def fast_closest_pair(cluster_list):
 
     Returns a tuple (distance, idx1, idx2) with idx1 < idx 2 where cluster_list[idx1] and cluster_list[idx2] have the
     smallest distance dist of any pair of clusters
+    :rtype : tuple
+    :param cluster_list:
     """
 
-    def fast_helper(cluster_list, horiz_order, vert_order):
+    def fast_helper(c_list, h_order, v_order):
         """
         Divide and conquer method for computing distance between closest pair of points Running time is O(n * log(n))
 
@@ -77,39 +80,44 @@ def fast_closest_pair(cluster_list):
         Returns a tuple (distance, idx1, idx2) with idx1 < idx 2 where cluster_list[idx1] and cluster_list[idx2]
         have the smallest distance dist of any pair of clusters
 
+        :rtype : tuple
+        :type c_list: list
+        :param c_list:
+        :param h_order:
+        :param v_order:
         """
 
         # base case
-        if len(horiz_order) <= 3:
+        if len(h_order) <= 3:
             # use brute force method on such a small set
-            min_pair = slow_closest_pairs([cluster_list[dummy_ctr] for dummy_ctr in horiz_order]).pop()
+            min_pair = slow_closest_pairs([c_list[dummy_ctr] for dummy_ctr in h_order]).pop()
 
-            return (min_pair[0], horiz_order[min_pair[1]], horiz_order[min_pair[2]])
+            return min_pair[0], h_order[min_pair[1]], h_order[min_pair[2]]
 
         # divide
         else:
             # calculate midpoint and middle x value
-            h_mid = len(horiz_order) / 2
-            mid_x = 0.5 * (cluster_list[horiz_order[h_mid - 1]].horiz_center() +
-                           cluster_list[horiz_order[h_mid]].horiz_center())
+            h_mid = len(h_order) / 2
+            mid_x = 0.5 * (c_list[h_order[h_mid - 1]].horiz_center() +
+                           c_list[h_order[h_mid]].horiz_center())
 
             # half the x-coordinates
-            h_l = horiz_order[:h_mid]
-            h_r = horiz_order[h_mid:]
+            h_l = h_order[:h_mid]
+            h_r = h_order[h_mid:]
 
             # half the y-coordinates (ordered)
             # TODO: make this faster
-            vlidx = [(cluster_list[dummy_ctr].vert_center(), dummy_ctr) for dummy_ctr in h_l]
+            vlidx = [(c_list[dummy_ctr].vert_center(), dummy_ctr) for dummy_ctr in h_l]
             vlidx.sort()
             v_l = [vlidx[dummy_ctr][1] for dummy_ctr in range(len(vlidx))]
 
-            vridx = [(cluster_list[dummy_ctr].vert_center(), dummy_ctr) for dummy_ctr in h_r]
+            vridx = [(c_list[dummy_ctr].vert_center(), dummy_ctr) for dummy_ctr in h_r]
             vridx.sort()
             v_r = [vridx[dummy_ctr][1] for dummy_ctr in range(len(vridx))]
 
             # break the problem into smaller bits
-            pair_l = fast_helper(cluster_list, h_l, v_l)
-            pair_r = fast_helper(cluster_list, h_r, v_r)
+            pair_l = fast_helper(c_list, h_l, v_l)
+            pair_r = fast_helper(c_list, h_r, v_r)
 
             # conquer
             if pair_l[0] < pair_r[0]:
@@ -119,13 +127,13 @@ def fast_closest_pair(cluster_list):
 
             # check around the mid point of x
             s_list = [dummy_ctr
-                      for dummy_ctr in vert_order
-                      if abs(cluster_list[dummy_ctr].horiz_center() - mid_x) < min_pair[0]]
+                      for dummy_ctr in v_order
+                      if abs(c_list[dummy_ctr].horiz_center() - mid_x) < min_pair[0]]
 
             s_size = len(s_list)
             for dummy_idx in xrange(s_size - 1):
                 for dummy_idy in xrange(dummy_idx + 1, min(dummy_idx + 4, s_size)):
-                    s_d = cluster_list[s_list[dummy_idx]].distance(cluster_list[s_list[dummy_idy]])
+                    s_d = c_list[s_list[dummy_idx]].distance(c_list[s_list[dummy_idy]])
                     if s_d < min_pair[0]:
                         min_pair = (s_d, s_list[dummy_idx], s_list[dummy_idy])
 
@@ -146,12 +154,15 @@ def fast_closest_pair(cluster_list):
     # compute answer recursively
     answer = fast_helper(cluster_list, horiz_order, vert_order)
 
-    return (answer[0], min(answer[1:]), max(answer[1:]))
+    return answer[0], min(answer[1:]), max(answer[1:])
 
 
 def hierarchical_clustering(cluster_list, num_clusters):
     """
     Compute a hierarchical clustering of a set of clusters
+    :rtype : list
+    :param cluster_list:
+    :param num_clusters:
     Note: the function mutates cluster_list
 
     Input: List of clusters, number of clusters
@@ -165,6 +176,10 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
     """
     Compute the k-means clustering of a set of clusters
 
+    :rtype : list
+    :param cluster_list:
+    :param num_clusters:
+    :param num_iterations:
     Input: List of clusters, number of clusters, number of iterations
     Output: List of clusters whose length is num_clusters
     """
@@ -172,19 +187,3 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
     # initialize k-means clusters to be initial clusters with largest populations
 
     return []
-
-
-# def compare_slow_fast_pairs():
-#     from random import randint
-#     import cProfile
-#
-#     pointList = [alg_cluster.Cluster(set(), randint(0,1000), randint(0,1000), 0, 0) for i in range(1000)]
-#
-#     for i in range(1):
-#         cProfile.run('fast_closest_pair(pointList)')
-#         cProfile.run('slow_closest_pairs(pointList)')
-
-
-# print fast_closest_pair([alg_cluster.Cluster(set([]), 90.9548590217, -17.089022585, 1, 0), alg_cluster.Cluster(set([]), 90.2536656675, -70.5911544718, 1, 0), alg_cluster.Cluster(set([]), -57.5872347006, 99.7124028905, 1, 0), alg_cluster.Cluster(set([]), -15.9338519877, 5.91547495626, 1, 0), alg_cluster.Cluster(set([]), 19.1869055492, -28.0681513017, 1, 0), alg_cluster.Cluster(set([]), -23.0752410653, -42.1353490324, 1, 0), alg_cluster.Cluster(set([]), -65.1732261872, 19.675582646, 1, 0), alg_cluster.Cluster(set([]), 99.7789872101, -11.2619165604, 1, 0), alg_cluster.Cluster(set([]), -43.3699854405, -94.7349852817, 1, 0), alg_cluster.Cluster(set([]), 48.2281912402, -53.3441788034, 1, 0)])
-# expected one of the tuples in set([(10.5745166749, 0, 7)])
-# received (38.500309620222225, 4, 9)
