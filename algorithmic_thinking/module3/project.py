55bae0d40ebe4e03efaad365c5e76242da5d03e1
hrpatel <pate3900+github@gmail.com>
2014-10-02 14:38:34 -0400

    code clean up, timer, test

--- algorithmic_thinking/module3/project.py
+++ algorithmic_thinking/module3/project.py
@@ -13,7 +13,6 @@ where cluster_list is a list of clusters in the plane
 __author__ = 'hrpatel'
 
 
-import math
 import alg_cluster
 
 
@@ -23,6 +22,7 @@ def pair_distance(cluster_list, idx1, idx2):
 
     Returns tuple (dist, idx1, idx2) with idx1 < idx2 where dist is distance between cluster_list[idx1]
     and cluster_list[idx2]
+    :rtype : tuple
     """
     return (cluster_list[idx1].distance(cluster_list[idx2]), min(idx1, idx2), max(idx1, idx2))
 
@@ -34,6 +34,7 @@ def slow_closest_pairs(cluster_list):
     Returns the set of all tuples of the form (dist, idx1, idx2) where the cluster_list[idx1] and cluster_list[idx2]
     have minimum distance dist.
 
+    :rtype : set
     """
     # initialize return variable
     pairs = set()
@@ -77,25 +78,18 @@ def fast_closest_pair(cluster_list):
         have the smallest distance dist of any pair of clusters
 
         """
-        # initialize size of horizontal points
-        h_size = len(horiz_order)
 
         # base case
-        if h_size <= 3:
+        if len(horiz_order) <= 3:
+            # use brute force method on such a small set
+            min_pair = slow_closest_pairs([cluster_list[dummy_ctr] for dummy_ctr in horiz_order]).pop()
 
-            small_list = []
-            for node in horiz_order:
-                small_list.append(cluster_list[node])
-
-            pair = slow_closest_pairs(small_list)
-            min_d, p_1, p_2 = pair.pop()
-
-            return (min_d, horiz_order[p_1], horiz_order[p_2])
+            return (min_pair[0], horiz_order[min_pair[1]], horiz_order[min_pair[2]])
 
         # divide
         else:
-            # midpoint
-            h_mid = h_size / 2
+            # calculate midpoint and middle x value
+            h_mid = len(horiz_order) / 2
             mid_x = 0.5 * (cluster_list[horiz_order[h_mid - 1]].horiz_center() +
                            cluster_list[horiz_order[h_mid]].horiz_center())
 
@@ -105,38 +99,37 @@ def fast_closest_pair(cluster_list):
 
             # half the y-coordinates (ordered)
             # TODO: make this faster
-            vlidx = [(cluster_list[idx].vert_center(), idx) for idx in h_l]
+            vlidx = [(cluster_list[dummy_ctr].vert_center(), dummy_ctr) for dummy_ctr in h_l]
             vlidx.sort()
-            v_l = [vlidx[idx][1] for idx in range(len(vlidx))]
+            v_l = [vlidx[dummy_ctr][1] for dummy_ctr in range(len(vlidx))]
 
-            vridx = [(cluster_list[idx].vert_center(), idx) for idx in h_r]
+            vridx = [(cluster_list[dummy_ctr].vert_center(), dummy_ctr) for dummy_ctr in h_r]
             vridx.sort()
-            v_r = [vridx[idx][1] for idx in range(len(vridx))]
+            v_r = [vridx[dummy_ctr][1] for dummy_ctr in range(len(vridx))]
 
             # break the problem into smaller bits
-            d_l, i_l, j_l = fast_helper(cluster_list, h_l, v_l)
-            d_r, i_r, j_r = fast_helper(cluster_list, h_r, v_r)
+            pair_l = fast_helper(cluster_list, h_l, v_l)
+            pair_r = fast_helper(cluster_list, h_r, v_r)
 
             # conquer
-            if d_l < d_r:
-                min_d, p_1, p_2 = d_l, i_l, j_l
+            if pair_l[0] < pair_r[0]:
+                min_pair = pair_l
             else:
-                min_d, p_1, p_2 = d_r, i_r, j_r
+                min_pair = pair_r
 
             # check around the mid point of x
-            s_list = []
-            for v_node in vert_order:
-                if abs(cluster_list[v_node].horiz_center() - mid_x) < min_d:
-                    s_list.append(v_node)
+            s_list = [dummy_ctr
+                      for dummy_ctr in vert_order
+                      if abs(cluster_list[dummy_ctr].horiz_center() - mid_x) < min_pair[0]]
 
             s_size = len(s_list)
-            for idx in xrange(s_size - 1):
-                for idy in xrange(idx + 1, min(idx + 4, s_size)):
-                    s_d = cluster_list[s_list[idx]].distance(cluster_list[s_list[idy]])
-                    if s_d < min_d:
-                        min_d, p_1, p_2 = s_d, s_list[idx], s_list[idy]
+            for dummy_idx in xrange(s_size - 1):
+                for dummy_idy in xrange(dummy_idx + 1, min(dummy_idx + 4, s_size)):
+                    s_d = cluster_list[s_list[dummy_idx]].distance(cluster_list[s_list[dummy_idy]])
+                    if s_d < min_pair[0]:
+                        min_pair = (s_d, s_list[dummy_idx], s_list[dummy_idy])
 
-        return (min_d, p_1, p_2)
+        return min_pair
 
     # compute list of indices for the clusters ordered in the horizontal direction
     hcoord_and_index = [(cluster_list[idx].horiz_center(), idx)
@@ -179,3 +172,19 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
     # initialize k-means clusters to be initial clusters with largest populations
 
     return []
+
+
+# def compare_slow_fast_pairs():
+#     from random import randint
+#     import cProfile
+#
+#     pointList = [alg_cluster.Cluster(set(), randint(0,1000), randint(0,1000), 0, 0) for i in range(1000)]
+#
+#     for i in range(1):
+#         cProfile.run('fast_closest_pair(pointList)')
+#         cProfile.run('slow_closest_pairs(pointList)')
+
+
+# print fast_closest_pair([alg_cluster.Cluster(set([]), 90.9548590217, -17.089022585, 1, 0), alg_cluster.Cluster(set([]), 90.2536656675, -70.5911544718, 1, 0), alg_cluster.Cluster(set([]), -57.5872347006, 99.7124028905, 1, 0), alg_cluster.Cluster(set([]), -15.9338519877, 5.91547495626, 1, 0), alg_cluster.Cluster(set([]), 19.1869055492, -28.0681513017, 1, 0), alg_cluster.Cluster(set([]), -23.0752410653, -42.1353490324, 1, 0), alg_cluster.Cluster(set([]), -65.1732261872, 19.675582646, 1, 0), alg_cluster.Cluster(set([]), 99.7789872101, -11.2619165604, 1, 0), alg_cluster.Cluster(set([]), -43.3699854405, -94.7349852817, 1, 0), alg_cluster.Cluster(set([]), 48.2281912402, -53.3441788034, 1, 0)])
+# expected one of the tuples in set([(10.5745166749, 0, 7)])
+# received (38.500309620222225, 4, 9)
